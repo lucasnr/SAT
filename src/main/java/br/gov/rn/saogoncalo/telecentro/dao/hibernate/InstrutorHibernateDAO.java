97ea0e26d84cdbda9867c3e45e91e5ce048f003b
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-10-17 13:15:33 -0300

    Consertando o cadastro de um instrutor

--- src/main/java/br/gov/rn/saogoncalo/telecentro/dao/hibernate/InstrutorHibernateDAO.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/dao/hibernate/InstrutorHibernateDAO.java
@@ -1,7 +1,11 @@
 package br.gov.rn.saogoncalo.telecentro.dao.hibernate;
 
+import org.hibernate.Session;
+
 import br.gov.rn.saogoncalo.telecentro.dao.InstrutorDAO;
+import br.gov.rn.saogoncalo.telecentro.model.Contato;
 import br.gov.rn.saogoncalo.telecentro.model.Instrutor;
+import br.gov.rn.saogoncalo.telecentro.model.Pessoa;
 
 public class InstrutorHibernateDAO extends UsuarioHibernateDAO<Instrutor> implements InstrutorDAO {
 
@@ -9,4 +13,20 @@ public class InstrutorHibernateDAO extends UsuarioHibernateDAO<Instrutor> implem
 		super(Instrutor.class);
 	}
 
+	@Override
+	public boolean salvar(Instrutor obj) {
+		Session session = super.sessionFactory.openSession();
+		session.getTransaction().begin();
+		
+		Pessoa pessoa = obj.getPessoa();
+		session.save(pessoa.getEndereco());
+		session.save(pessoa);
+		Contato contato = pessoa.getContato();
+		contato.setId(pessoa.getId());
+		session.save(contato);
+		session.save(obj);
+		
+		session.getTransaction().commit();
+		return true;
+	}
 }
