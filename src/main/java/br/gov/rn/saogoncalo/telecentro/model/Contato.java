79e2e697d48041ced46a74817ad1d9fc8841e812
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-10-17 13:15:57 -0300

    Adicionando Builder a classe Contanto

--- src/main/java/br/gov/rn/saogoncalo/telecentro/model/Contato.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/model/Contato.java
@@ -6,12 +6,14 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "CONTATO")
 public class Contato {
