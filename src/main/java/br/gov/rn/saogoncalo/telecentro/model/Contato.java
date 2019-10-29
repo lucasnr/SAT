a1ecb2451ce5f5609d66fc4d7b07b8fd4a9d568f
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-10-29 13:22:32 -0300

    Adicionando builder ao contato

--- src/main/java/br/gov/rn/saogoncalo/telecentro/model/Contato.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/model/Contato.java
@@ -5,20 +5,19 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "CONTATO")
 public class Contato {
 
 	@Id
-	@Column(name = "PESSOA_ID", columnDefinition="INT")
+	@Column(name = "PESSOA_ID", columnDefinition = "INT")
 	private Long id;
 	private String email;
 	private String telefone;
+
 }
