63e93930918caf8c6bfc77067b219e50eaa2156e
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-10-29 13:23:27 -0300

    Criando service do endereço

---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/service/EnderecoService.java
@@ -0,0 +1,8 @@
+package br.gov.rn.saogoncalo.telecentro.service;
+
+import br.gov.rn.saogoncalo.telecentro.model.Endereco;
+
+public interface EnderecoService {
+
+	public boolean salvar(Endereco endereco);
+}
---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/EnderecoServiceImpl.java
@@ -0,0 +1,33 @@
+package br.gov.rn.saogoncalo.telecentro.service.cdi;
+
+import javax.inject.Inject;
+
+import org.hibernate.exception.ConstraintViolationException;
+
+import br.gov.rn.saogoncalo.telecentro.dao.EnderecoDAO;
+import br.gov.rn.saogoncalo.telecentro.model.Endereco;
+import br.gov.rn.saogoncalo.telecentro.service.EnderecoService;
+
+public class EnderecoServiceImpl implements EnderecoService {
+
+	private static final int DUPLICATE_CONSTRAINT = 1062;
+	private static final int NULL_COLUMN = 1048;
+	
+	@Inject
+	private EnderecoDAO dao;
+	
+	@Override
+	public boolean salvar(Endereco endereco) {
+		try {
+			dao.salvar(endereco);
+			return true;
+		} catch(ConstraintViolationException e) {
+			if(e.getErrorCode() == NULL_COLUMN)
+				throw new IllegalArgumentException("Algum campo não foi especificado");
+			else if(e.getErrorCode() == DUPLICATE_CONSTRAINT) 
+				throw new IllegalArgumentException("O endereço já existe");
+		}
+		return false;
+	}
+	
+}
