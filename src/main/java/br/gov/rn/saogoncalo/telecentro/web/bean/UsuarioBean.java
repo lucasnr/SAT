a6d4a3c775883f5e363c67cb024f530cec20a059
Guilllps <56686098+Guilllps@users.noreply.github.com>
2019-11-18 17:16:20 -0300

    Editando a classe UsuarioBean
    Produção do método buscaPorId

---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/web/bean/UsuarioBean.java
@@ -0,0 +1,41 @@
+package br.gov.rn.saogoncalo.telecentro.web.bean;
+
+
+import java.util.Optional;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.gov.rn.saogoncalo.telecentro.model.Usuario;
+import br.gov.rn.saogoncalo.telecentro.service.UsuarioService;
+import br.gov.rn.saogoncalo.telecentro.util.FacesMessageUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+@Named
+@RequestScoped
+public class UsuarioBean {
+
+	@Inject
+	private UsuarioService service;
+
+	@Getter @Setter
+	private Long idconsultado;
+	
+	@Getter @Setter
+	private Usuario usuario;
+
+	public String buscarPorId() {
+		
+		Optional<Usuario> optional = service.buscarPorId(idconsultado);
+		if (optional.isPresent()) {
+			usuario = optional.get();
+		} else {
+			FacesMessageUtil.addErrorMessage("Nao encontroei");
+		}
+		
+		return "atualizardadosusuario.xhtml?faces-redirect=true&includeViewParams=true";
+	}
+
+}
