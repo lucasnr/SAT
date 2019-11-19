6768e17d769d4c79435cd19c759108db91d0964e
Anderson Lucio <56683221+andersonslucio@users.noreply.github.com>
2019-11-19 18:02:31 -0300

    Criando Bean e Validador para a página de cadastro de usuários

---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/web/bean/CadastrarUsuarioBean.java
@@ -0,0 +1,62 @@
+package br.gov.rn.saogoncalo.telecentro.web.bean;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.gov.rn.saogoncalo.telecentro.model.Unidade;
+import br.gov.rn.saogoncalo.telecentro.model.Usuario;
+import br.gov.rn.saogoncalo.telecentro.service.UnidadeService;
+import br.gov.rn.saogoncalo.telecentro.service.UsuarioService;
+import lombok.Getter;
+import lombok.Setter;
+
+@Named
+@RequestScoped
+public class CadastrarUsuarioBean {
+	
+	@Getter @Setter
+	private Usuario usuario;
+	
+	@Getter @Setter
+	private Date dataNascimento;
+	
+	public CadastrarUsuarioBean() {
+		usuario = new Usuario();
+	}
+	
+	@Inject
+	private UsuarioService service;
+	
+	public void salvar() {
+		LocalDate localDate = dataNascimento.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();;
+		usuario.setDataNascimento(localDate);
+		System.out.println(usuario);
+		
+//		boolean salvou = service.salvar(usuario);
+//		
+//		if(salvou) {
+//			System.out.println("Deu certo");
+//		} else {
+//			System.out.println("Deu ruim");
+//		}
+	}
+	
+
+	@Inject
+	private UnidadeService unidadeService;
+
+	public List<Unidade> unidades() {
+		return unidadeService.listar();
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/web/validator/EmailValidator.java
@@ -0,0 +1,22 @@
+package br.gov.rn.saogoncalo.telecentro.web.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+
+@FacesValidator("emailValidator")
+public class EmailValidator implements Validator {
+
+	@Override
+	public void validate(FacesContext context, UIComponent toValidate, Object value) {
+		String email = (String) value;
+		if (email.indexOf('@') == -1) {
+			((UIInput) toValidate).setValid(false);
+			FacesMessage message = new FacesMessage("Informe um email válido");
+			context.addMessage(toValidate.getClientId(context), message);
+		}
+	}
+}
