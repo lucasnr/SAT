f150fa7b1b110b44c84fa18d8e92b3eb4ced64d5
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-11-20 18:42:21 -0200

    Adicionando metodo de busca por id no service da unidade

--- src/main/java/br/gov/rn/saogoncalo/telecentro/service/UnidadeService.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/service/UnidadeService.java
@@ -1,11 +1,13 @@
 package br.gov.rn.saogoncalo.telecentro.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.gov.rn.saogoncalo.telecentro.model.Unidade;
 
 public interface UnidadeService {
 
 	public boolean salvar(Unidade unidade);
+	public Optional<Unidade> buscarPorId(Long id);
 	public List<Unidade> listar();
 }
--- src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/UnidadeServiceImpl.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/UnidadeServiceImpl.java
@@ -2,6 +2,7 @@ package br.gov.rn.saogoncalo.telecentro.service.cdi;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
@@ -15,6 +16,7 @@ public class UnidadeServiceImpl implements UnidadeService, Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	@Inject
 	private UnidadeDAO dao;
 
@@ -26,6 +28,11 @@ public class UnidadeServiceImpl implements UnidadeService, Serializable {
 	@Override
 	public List<Unidade> listar() {
 		return dao.listar(0, 0);
+	}
+
+	@Override
+	public Optional<Unidade> buscarPorId(Long id) {
+		return dao.buscarPorPK(id);
 	}
 	
 }
