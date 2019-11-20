1a85b91501bb037a311baa64c79b913874820b7e
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-11-20 18:49:56 -0200

    Fazendo as implementações dos service implementarem Serializable

--- src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/EnderecoServiceImpl.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/EnderecoServiceImpl.java
@@ -1,13 +1,19 @@
 package br.gov.rn.saogoncalo.telecentro.service.cdi;
 
+import java.io.Serializable;
+
 import javax.inject.Inject;
 
 import br.gov.rn.saogoncalo.telecentro.dao.EnderecoDAO;
 import br.gov.rn.saogoncalo.telecentro.model.Endereco;
 import br.gov.rn.saogoncalo.telecentro.service.EnderecoService;
 
-public class EnderecoServiceImpl implements EnderecoService {
+public class EnderecoServiceImpl implements EnderecoService, Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Inject
 	private EnderecoDAO dao;
 	
