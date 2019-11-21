56bf9d04afdb89a69cd9027390809b5a25d1b351
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-11-21 21:09:10 -0200

    Criando dao do boletim

---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/dao/BoletimDAO.java
@@ -0,0 +1,7 @@
+package br.gov.rn.saogoncalo.telecentro.dao;
+
+import br.gov.rn.saogoncalo.telecentro.model.Boletim;
+
+public interface BoletimDAO extends AbstractDAO<Boletim, Boletim.BoletimId> {
+
+}
---
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/dao/hibernate/BoletimHibernateDAO.java
@@ -0,0 +1,13 @@
+package br.gov.rn.saogoncalo.telecentro.dao.hibernate;
+
+import br.gov.rn.saogoncalo.telecentro.dao.BoletimDAO;
+import br.gov.rn.saogoncalo.telecentro.model.Boletim;
+
+public class BoletimHibernateDAO extends AbstractHibernateDAO<Boletim, Boletim.BoletimId> implements BoletimDAO {
+
+	public BoletimHibernateDAO() {
+		super(Boletim.class);
+	}
+
+	
+}
