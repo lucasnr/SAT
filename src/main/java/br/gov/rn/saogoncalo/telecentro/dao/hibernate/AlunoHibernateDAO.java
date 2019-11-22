48d33d8cde1fe8e825e2e6ac6272159817bc19ec
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-11-22 12:24:24 -0200

    Criando metodo de atualizar no dao do aluno

--- src/main/java/br/gov/rn/saogoncalo/telecentro/dao/hibernate/AlunoHibernateDAO.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/dao/hibernate/AlunoHibernateDAO.java
@@ -18,10 +18,24 @@ public class AlunoHibernateDAO extends UsuarioHibernateDAO<Aluno> implements Alu
 	public Optional<Aluno> buscarUltimoAlunoDaTurma(String codigoDaTurma) {
 		Session session = getSession();
 		
-		Query<Aluno> query = session.createQuery("from Aluno a where a.turma.codigo = :codigo order by dataCadastro desc limit 1", Aluno.class);
+		Query<Aluno> query = session.createQuery("select a from Aluno a join Pessoa p on p.id = a.pessoa.id where a.turma.codigo = :codigo order by p.dataCadastro desc", Aluno.class);
 		query.setParameter("codigo", codigoDaTurma);
-		Aluno resultado = query.uniqueResult();
+		Aluno resultado = query.setMaxResults(1).uniqueResult();
 		return Optional.ofNullable(resultado);
 	}
 
+	@Override
+	public boolean atualizar(Aluno aluno) {
+		Session session = super.sessionFactory.getCurrentSession();
+		session.getTransaction().begin();
+		Query<?> query = session.createQuery("update Aluno a SET a.matricula = :matricula, a.turma = :turma where a.id = :id");
+		query.setParameter("matricula", aluno.getMatricula());
+		query.setParameter("turma", aluno.getTurma());
+		query.setParameter("id", aluno.getUsuarioId());
+		int linhasAlteradas = query.executeUpdate();
+		session.getTransaction().commit();
+		session.close();
+		return linhasAlteradas == 1;
+	}
+	
 }
