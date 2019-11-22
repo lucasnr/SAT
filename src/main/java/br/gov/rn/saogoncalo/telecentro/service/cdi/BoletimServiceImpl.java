d70c16ac28e349fd10691944fecf2e678eca0562
echiberio <56694864+echiberio@users.noreply.github.com>
2019-11-22 17:59:59 -0300

    Impletando Serializable

--- src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/BoletimServiceImpl.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/service/cdi/BoletimServiceImpl.java
@@ -1,5 +1,6 @@
 package br.gov.rn.saogoncalo.telecentro.service.cdi;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,8 +9,12 @@ import br.gov.rn.saogoncalo.telecentro.dao.BoletimDAO;
 import br.gov.rn.saogoncalo.telecentro.model.Boletim;
 import br.gov.rn.saogoncalo.telecentro.service.BoletimService;
 
-public class BoletimServiceImpl implements BoletimService {
+public class BoletimServiceImpl implements BoletimService, Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Inject
 	private BoletimDAO dao;
 	
