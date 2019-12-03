d2a05477d452bcf0d27a4cf16bf18e90be1b6bbd
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-12-03 12:55:04 -0200

    Resolvendo atribuição de frequencias
    Co-Authored-By: daniielqueiroz <daniielqueiroz@users.noreply.github.com>

--- src/main/java/br/gov/rn/saogoncalo/telecentro/web/bean/AtribuirFrequenciaBean.java
+++ src/main/java/br/gov/rn/saogoncalo/telecentro/web/bean/AtribuirFrequenciaBean.java
@@ -1,55 +1,68 @@
 package br.gov.rn.saogoncalo.telecentro.web.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import br.gov.rn.saogoncalo.telecentro.model.Turma;
-import br.gov.rn.saogoncalo.telecentro.service.TurmaService;
+import br.gov.rn.saogoncalo.telecentro.model.Aluno;
+import br.gov.rn.saogoncalo.telecentro.model.Aula;
+import br.gov.rn.saogoncalo.telecentro.model.RegistroAula;
+import br.gov.rn.saogoncalo.telecentro.service.AulaService;
+import br.gov.rn.saogoncalo.telecentro.service.RegistroAulaService;
+import br.gov.rn.saogoncalo.telecentro.util.FacesMessageUtil;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Named
 @ViewScoped
-public class AtribuirFrequenciaBean implements Serializable{
+public class AtribuirFrequenciaBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	
-	
+
 	@Getter
 	@Setter
 	private Long id;
-	
-	@Getter
-	@Setter	
-	private Long aulaId;
-	
+
 	@Getter
 	@Setter
-	private Turma turma;
-	
+	private Aula aula;
+
+	@Getter
+	@Setter
+	private List<RegistroAula> registrosAula;
+
 	@Inject
-	private TurmaService service;
-	
-	
-	public void carregarTurmaPorId() {
-		Optional<Turma> optional = service.buscarPorId(id);
-		if(optional.isPresent()) {
-			turma = optional.get();
+	private AulaService aulaService;
+
+	public void carregarAulaPorId() {
+		Optional<Aula> optional = aulaService.buscarPorId(id);
+		if (optional.isPresent()) {
+			this.aula = optional.get();
+			this.registrosAula = new ArrayList<>();
+			for (Aluno aluno : aula.getTurma().getAlunos()) {
+				RegistroAula registroAula = new RegistroAula(aluno, aula);
+				registrosAula.add(registroAula);
+			}
 		} else {
-			//nao encontrou a turma com o id
+			// nao encontrou a turma com o id
 		}
 	}
-	
+
+	@Inject
+	private RegistroAulaService service;
+
 	public void atribuirFrequencia() {
-		
+		boolean sucesso = service.salvarRegistros(registrosAula);
+		if (sucesso) {
+			FacesMessageUtil.addSuccessMessage("Frequencias atribuidas com sucesso");
+		} else {
+			FacesMessageUtil.addErrorMessage("Erro ao atribuir frequencias");
+		}
 	}
-	
-	
-	
+
 }
